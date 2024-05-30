a1ad32e2ff8498894f268801f8e954d4cc8e62c3
Iacopo Ciao <iacopociao1992@gmail.com>
2024-05-30 10:33:15 +0200

    chore: moved operations in cli

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -3,26 +3,8 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
-  final workspaceName = context.vars['workspaceName'].toString();
-  if (workspaceName.trim().isEmpty) {
-    throw Exception('Missing valid workspace name');
-  }
-
   await runDotenvVault(
     context: context,
-  );
-
-  await runPnpm(
-    context: context,
-  );
-
-  await runGit(
-    context: context,
-  );
-
-  runNextSteps(
-    context: context,
-    workspaceName: workspaceName,
   );
 }
 
@@ -48,41 +30,4 @@ Future<void> runDotenvVault({
   );
 
   context.logger.success('🔑 .env Vault configured successfully 🚀');
-}
-
-Future<void> runPnpm({
-  required HookContext context,
-}) async {
-  context.logger.info('📦 Running pnpm i');
-  await Process.run('pnpm', ['i']);
-  context.logger.success('📦 pnpm configured successfully 🚀');
-}
-
-Future<void> runGit({
-  required HookContext context,
-}) async {
-  context.logger.info('📚 Initializing git with "main" as default branch...');
-  await Process.run(
-    'git',
-    ['init', '--initial-branch=main'],
-  );
-
-  context.logger.info('📚 Staging initial scaffold files...');
-  await Process.run('git', ['add', '.'],);
-
-  context.logger.info('📚 Committing "chore(scaffold): initial commit"...');
-  await Process.run(
-    'git',
-    ['commit', '-m', '"chore(scaffold): initial commit"'],
-  );
-
-  context.logger.success('📚 Git initialized successfully! 🚀');
-}
-
-
-runNextSteps({required HookContext context, required String workspaceName}) {
-  context.logger.success('🏛️ Scaffold initialized successfully! 🚀');
-  context.logger.info('Next steps:');
-  context.logger.info('  cd ${workspaceName.paramCase}');
-  context.logger.info('  pnpm run start');
 }
